db2cd8902020cfa3e224766a27f1ff21bb720b81
Eugenia Galkina <yavsehmilee@gmail.com>
2022-04-26 18:59:06 +0300

    поиск макс из трех

---
+++ part3/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("введите три числа: ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+int c = int.Parse(Console.ReadLine());
+
+int max = a;
+
+if (b>max) max = b;
+
+if (c>max) max = c;
+
+int result = max;
+Console.Write("Большее число: ");
+Console.WriteLine (result);
+

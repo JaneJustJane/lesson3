8c6d1b6934599a18b8fb65575d79ca7159fd8d18
Eugenia Galkina <yavsehmilee@gmail.com>
2022-04-26 19:24:23 +0300

    ряд чисел между двумя заданными

---
+++ part4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("введите два числа: ");
+int a = int.Parse(Console.ReadLine());
+int b = int.Parse(Console.ReadLine());
+
+while (a<=b)
+{
+    Console.Write (a + " ");
+    a=a+1;
+}

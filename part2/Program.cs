1b6d1217773d7f6501b6640d55c6cf94c5c14fec
Eugenia Galkina <yavsehmilee@gmail.com>
2022-04-26 18:09:45 +0300

    проба программы поиска большего числа из двух

---
+++ part2/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("введите число a: ");
+string userString = Console.ReadLine();
+int a = Convert.ToInt32(userString);
+Console.Write("введите число b: ");
+string userString = Console.ReadLine();
+int b = Convert.ToInt32(userString);
+
+if (a>b)
+{
+    int result = a;
+    Console.Write("Большее число: ");
+    Console.WriteLine (result);
+}
+else
+{
+    int result = b;
+    Console.Write("Большее число: ");
+    Console.WriteLine (result);
+}
+
